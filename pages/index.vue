b5ff587b8b4f94b3cff06c78a8576f53c51dba30
kokoa0429 <kokoa@ciebus.net>
2021-02-27 04:46:19 +0900

    Add notification page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
-  div Hello!!
+  div
+    v-alert(border="top" colored-border="" type="info" elevation=2)
+      nuxt-link(to="/notification")
+       | 密告中運営からのお知らせがあります。
 </template>
 
 <script>
---
+++ pages/notification.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  div 通知だよ
+</template>
+
+<script>
+export default {
+  name: 'notification',
+}
+</script>
+
+<style scoped></style>
