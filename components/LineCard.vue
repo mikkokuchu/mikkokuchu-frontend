456b9cc83e2deb2cd5370b54abf79aeef518a9e5
kokoa0429 <kokoa@ciebus.net>
2021-02-28 01:18:17 +0900

    Add LineCard

---
+++ components/LineCard.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  a(style="width:300px;height:300px;" href="https://lin.ee/mANdjB7")
+    img(src="/line-button.png" width="300px")
+</template>
+
+<script>
+export default {
+  name: 'LineCard',
+}
+</script>
+
+<style scoped></style>
