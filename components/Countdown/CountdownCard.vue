9e5b1557e4bba584ec76d8c831807ef5156e9c73
kokoa0429 <kokoa@ciebus.net>
2021-02-28 01:19:21 +0900

    Add CountdownCard

---
+++ components/Countdown/CountdownCard.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  v-card
+    v-card-title {{ game.title }} {{ new Date().getTime() < game.startTime ? "開始" : "終了" }} まで残り:
+      vue-countdown(v-if="new Date().getTime() < game.startTime" :time="game.startTime - new Date().getTime()" v-slot="{ days, hours, minutes, seconds }")
+        | {{ days }}日{{ hours }}時間{{ minutes }}分{{ seconds }}秒
+      vue-countdown(v-else :time="game.endTime - new Date().getTime()" v-slot="{ days, hours, minutes, seconds }")
+        | {{ days }}日{{ hours }}時間{{ minutes }}分{{ seconds }}秒
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'CountdownCard',
+  computed: {
+    ...mapState('game/game', ['game']),
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ plugins/vue-countdown.js
@@ -0,0 +1,6 @@
+import VueCountdown from '@chenfengyuan/vue-countdown'
+import Vue from 'vue'
+
+Vue.use(VueCountdown)
+
+Vue.component('VueCountdown', VueCountdown)
