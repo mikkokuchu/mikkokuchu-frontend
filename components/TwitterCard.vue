eb4a2c9d78ae5e3848b39ae2ac07e5187e85d064
kokoa0429 <kokoa@ciebus.net>
2021-02-28 01:18:45 +0900

    Add TwitterCard

---
+++ components/TwitterCard.vue
@@ -0,0 +1,23 @@
+<template style="height: 500px">
+  <div style="height: 500px">
+    <a
+      class="twitter-timeline"
+      data-height="500"
+      href="https://twitter.com/Mikkokuchu?ref_src=twsrc%5Etfw"
+      >Tweets by Mikkokuchu</a
+    >
+    <script
+      async
+      src="https://platform.twitter.com/widgets.js"
+      charset="utf-8"
+    ></script>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TwitterCard',
+}
+</script>
+
+<style scoped></style>
