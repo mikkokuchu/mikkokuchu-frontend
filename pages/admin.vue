f21dc5c8815f1090ed86092e58e6f96f03e7e38d
kokoa0429 <kokoa@ciebus.net>
2021-02-28 07:18:03 +0900

    Update Admin page

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,18 +1,25 @@
 <template lang="pug">
   div
-    admin-user-table
+    admin-user-table.mb-2
+    admin-mission-table
+    user-dialog
+    mission-dialog
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import UserDialog from '~/components/Admin/UserDialog'
+import MissionDialog from '~/components/Admin/MissionDialog'
 
 export default {
   name: 'admin',
+  components: { UserDialog, MissionDialog },
   methods: {
-    ...mapActions('admin/admin', ['updateUserInfo']),
+    ...mapActions('admin/admin', ['updateUserInfo', 'updateMissionsInfo']),
   },
   mounted() {
     this.updateUserInfo()
+    this.updateMissionsInfo()
   },
 }
 </script>
--- store/admin/admin.js
+++ store/admin/admin.js
@@ -1,9 +1,73 @@
 export const state = () => ({
   users: [],
+  missions: [],
+  selectUser: {},
+  selectMission: {},
+  showUserDialog: false,
+  showMissionDialog: false,
+  userDialogId: null,
+  missionDialogId: null,
 })
 export const mutations = {
   setUsersInfo(state, data) {
     state.users = data
+  },
+  setMissionsInfo(state, data) {
+    state.missions = data
+  },
+  setUserDialogId(state, data) {
+    console.log(data)
+    state.userDialogId = data
+    state.selectUser = state.users.find((p) => p.id === data)
+  },
+  setUserDialogShow(state, data) {
+    state.showUserDialog = data
+    if (!data) {
+      state.selectUser = {}
+      state.userDialogId = null
+    }
+  },
+
+  setMissionDialogId(state, data) {
+    console.log(data)
+    state.questDialogId = data
+    state.selectMission = state.missions.find((p) => p.id === data)
+  },
+  setMissionDialogShow(state, data) {
+    state.showMissionDialog = data
+    if (!data) {
+      state.selectMission = {}
+      state.missionDialogId = null
+    }
+  },
+
+  setUserName(state, data) {
+    state.selectUser.userName = data
+  },
+  setLifeCount(state, data) {
+    state.selectUser.lifeCount = data
+  },
+  setCode(state, data) {
+    state.selectUser.code = data
+  },
+
+  setMissionTitle(state, data) {
+    state.selectMission.title = data
+  },
+  setMissionDescription(state, data) {
+    state.selectMission.description = data
+  },
+  setMissionPenaltyText(state, data) {
+    state.selectMission.penaltyText = data
+  },
+  setMissionStartTime(state, data) {
+    state.selectMission.startTime = data
+  },
+  setMissionEndTime(state, data) {
+    state.selectMission.endTime = data
+  },
+  setMissionType(state, data) {
+    state.selectMission.missionType = data
   },
 }
 
@@ -11,5 +75,37 @@ export const actions = {
   async updateUserInfo({ commit, rootState }) {
     const usersInfo = await this.$axios.$get(rootState.endpoint + 'admin/user')
     commit('setUsersInfo', usersInfo)
+  },
+  async updateMissionsInfo({ commit, rootState }) {
+    const missionsInfo = await this.$axios.$get(
+      rootState.endpoint + 'admin/mission'
+    )
+    commit('setMissionsInfo', missionsInfo)
+  },
+  async saveUserInfo({ dispatch, commit, rootState, state }) {
+    await this.$axios.$post(
+      rootState.endpoint + 'admin/user/' + state.selectUser.id,
+      state.selectUser,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    commit('setUserDialogShow', false)
+    dispatch('updateUserInfo')
+  },
+  async saveMissionInfo({ dispatch, commit, rootState, state }) {
+    await this.$axios.$post(
+      rootState.endpoint + 'admin/mission/' + state.selectMission.id,
+      state.selectMission,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    commit('setMissionDialogShow', false)
+    dispatch('updateMissionsInfo')
   },
 }
