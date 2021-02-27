36033dbd027933a9ed8303fb8eceac31853c7413
kokoa0429 <kokoa@ciebus.net>
2021-02-28 03:30:18 +0900

    Add AdminUserTable

---
+++ components/Admin/AdminUserTable.vue
@@ -0,0 +1,69 @@
+<template lang="pug">
+  v-card
+    v-card-title ユーザ
+    v-card-text
+      v-data-table(:headers='headers' :items="users")
+        template(v-slot:item.actions="{item}")
+          v-icon(@click="test(item)") mdi-pencil
+        template(v-slot:item.joinTime="{item}")
+          | {{ getDateFormat(item.joinTime) }}
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'AdminUserTable',
+  data() {
+    return {
+      headers: [
+        {
+          text: 'ID',
+          align: 'start',
+          value: 'id',
+        },
+        {
+          text: '状態',
+          value: 'userStatus',
+        },
+        {
+          text: '名前',
+          value: 'userName',
+        },
+        {
+          text: 'ライフ数',
+          value: 'lifeCount',
+        },
+        {
+          text: 'コード',
+          value: 'code',
+        },
+        {
+          text: '参加日時',
+          value: 'joinTime',
+        },
+        { text: 'Actions', value: 'actions', sortable: false },
+      ],
+    }
+  },
+  computed: {
+    ...mapState('admin/admin', ['users']),
+  },
+  methods: {
+    test(arg) {
+      console.log(arg)
+    },
+    getDateFormat(date) {
+      const d = new Date(date)
+      console.log(d)
+      let str = ''
+      str += [d.getFullYear(), d.getMonth(), d.getDay()].join('/')
+      str += ' '
+      str += [d.getHours(), d.getMinutes(), d.getSeconds()].join(':')
+      return str
+    },
+  },
+}
+</script>
+
+<style scoped></style>
